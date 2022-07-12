d67038d342fde47bc2b92d4ca7e26d0c072de0c2
Harold <harold@pop-os.localdomain>
2022-07-12 02:41:12 -0400

    Added Test cases for uploadImages

--- src/contracts/Ethergram.sol
+++ src/contracts/Ethergram.sol
@@ -19,13 +19,26 @@ contract Ethergram {
     }
 
 
+    // EVENT happen after uploadImage is called
+    event ImageCreated( uint id, string hash_string, string description, uint tipAmount,address author);
+
     //====== Create Images =======
     function uploadImage(string memory _imgHash, string memory _description) public {
+        //Require imageHash to exist
+        require(bytes(_imgHash).length > 0, "ImageHash must exist");
+        // Require Uploaded Image to have Description
+        require(bytes(_description).length > 0, "Image must have description");
+        // Make sure Address of Uploader Exists
+        require(msg.sender != address(0x0), "Address of Uploader Must Exist");
+
         // Increment Image ID
         imageCount++;
 
         // Add Image To Contract (msg.sender is person who called contract)
         images[imageCount] = Image(imageCount, _imgHash, _description, 0, msg.sender);
+
+        // Return Result in ethergram.uploadImage.logs[0].args
+        emit ImageCreated(imageCount, _imgHash, _description, 0 , msg.sender);
         }
 
 
--- test/test.js
+++ test/test.js
@@ -32,19 +32,34 @@ contract('Ethergram', ([deployer, author, tipper]) => {
 
     // ============== IMAGES =============
     describe('IMAGES', async () => {
-      let result;
+      let result, test_imageCount;
+      const test_hash = 'abc123'
+      const test_description = 'Image description'
+      const test_tipAmount = '0'
+
+      before(async () => {
+        // ACTIONS
+        result = await ethergram.uploadImage(test_hash, test_description,  {from: author});
+        test_imageCount = await ethergram.imageCount()
+      })
       it('Create Images', async () => {
-        result = await ethergram.uploadImage()
-        let image = await ethergram.images(1);
-        console.log(image)
+        // SUCCESS
+        const event = result.logs[0].args // Get Info if Image Struct after calling uploadImage()
+        assert.equal(event.id.toNumber(), test_imageCount.toNumber(), 'id is correct')
+        assert.equal(event.hash_string, test_hash, 'Hash is correct')
+        assert.equal(event.description,test_description, 'Description is correct')
+        assert.equal(event.tipAmount,test_tipAmount, 'Tip Amount is correct')
+        assert.equal(event.author, author, 'Author is correct')
+
+        // FAILURE: Image must have hash
+        await ethergram.uploadImage('', test_description, {from:author}).should.be.rejected;
+        
       })
     })
     
 
 
-
-
      // ============ TIPPING =============
-     describe('IMAGES', async () => {
-    })
+    //  describe('IMAGES', async () => {
+    // })
 })
